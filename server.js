d0693f0237670a3f718b1b7c22237c52013fb768
Kevin Funk <kevin@kfunk.org>
2013-08-26 11:37:46 +0200

    Minor: Proper indentation

--- server.js
+++ server.js
@@ -142,88 +142,88 @@ var App = function() {
                 }
             });
         }
+    };
 
-        function renderResults_JSON(res, err, results) {
-            res.writeHead(200, {'Content-Type': 'application/json; charset=UTF-8'});
+    function renderResults_JSON(res, err, results) {
+        res.writeHead(200, {'Content-Type': 'application/json; charset=UTF-8'});
 
-            var json = {
-                error: err,
-                results: results
-            };
-            var jsonString = JSON.stringify(json);
-            res.write(jsonString);
-            res.end();
+        var json = {
+            error: err,
+            results: results
+        };
+        var jsonString = JSON.stringify(json);
+        res.write(jsonString);
+        res.end();
+    }
+
+    function renderResults_HTML(res, err, results) {
+        res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+
+        if (err) {
+            res.write('<h1>Error</h1>\n');
+            res.write('Error: ' + err);
+            res.end()
+            return
         }
 
-        function renderResults_HTML(res, err, results) {
-            res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-
-            if (err) {
-                res.write('<h1>Error</h1>\n');
-                res.write('Error: ' + err);
-                res.end()
-                return
+        res.write('<h1>Menus</h1\n');
+        res.write('<p></p>');
+        results.forEach(function(result) {
+            res.write("<h2>" + result.name + "</h2>\n");
+            if (result.error) {
+                res.write('<p>Error: ' + result.error + '</p>')
             }
 
-            res.write('<h1>Menus</h1\n');
-            res.write('<p></p>');
-            results.forEach(function(result) {
-                res.write("<h2>" + result.name + "</h2>\n");
-                if (result.error) {
-                    res.write('<p>Error: ' + result.error + '</p>')
-                }
+            res.write('Last update: ' + (result.date ? result.date : 'Unknown'));
 
-                res.write('Last update: ' + (result.date ? result.date : 'Unknown'));
+            // print entries
+            if (result.entries) {
+                res.write('<ul>\n');
+                result.entries.forEach(function(entry) {
+                    res.write("<li>" + entry + "</li>\n");
+                });
+                res.write("</ul>\n");
+            } else {
+                res.write('<p>No entries.</p>');
+            }
+        });
+        res.end();
+    }
 
-                // print entries
-                if (result.entries) {
-                    res.write('<ul>\n');
-                    result.entries.forEach(function(entry) {
-                        res.write("<li>" + entry + "</li>\n");
-                    });
-                    res.write("</ul>\n");
-                } else {
-                    res.write('<p>No entries.</p>');
-                }
+    function callResolver(resolver, callback) {
+        function createResult(resolver, data) {
+            result = {};
+            result.name = resolver.name;
+            result.link = resolver.link;
+            result.data = data;
+            return result;
+        }
+
+        req = http.request(resolver.request.options, function(res) {
+            var decoder = new StringDecoder('utf8');
+
+            var buffer = '';
+            res.on('data', function(chunk) {
+                buffer += decoder.write(chunk);
             });
-            res.end();
-        }
-
-        function callResolver(resolver, callback) {
-            function createResult(resolver, data) {
-                result = {};
-                result.name = resolver.name;
-                result.link = resolver.link;
-                result.data = data;
-                return result;
-            }
-
-            req = http.request(resolver.request.options, function(res) {
-                var decoder = new StringDecoder('utf8');
-
-                var buffer = '';
-                res.on('data', function(chunk) {
-                    buffer += decoder.write(chunk);
-                });
-                res.on('end', function() {
-                    data = resolver.parse(resolver, buffer);
-                    result = createResult(resolver, data);
-                    callback(null, result);
-                });
-            }).on('error', function(e) {
-                errorMessage = 'Failed to fetch URL: ' + url + '. Message: ' + e.message;
-                console.warn('Error: ' + errorMessage);
-                result = createResult(resolver, {error: errorMessage});
+            res.on('end', function() {
+                data = resolver.parse(resolver, buffer);
+                result = createResult(resolver, data);
                 callback(null, result);
             });
+        }).on('error', function(e) {
+            errorMessage = 'Failed to fetch URL: ' + url + '. Message: ' + e.message;
+            console.warn('Error: ' + errorMessage);
+            result = createResult(resolver, {error: errorMessage});
+            callback(null, result);
+        });
 
-            if (resolver.request.body) {
-                req.setHeader('Content-Length', resolver.request.body.length)
-                req.write(resolver.request.body);
-            }
-            req.end();
+        if (resolver.request.body) {
+            req.setHeader('Content-Length', resolver.request.body.length)
+            req.write(resolver.request.body);
         }
-    };
+        req.end();
+    }
 
 
     /**
